e268779417b88da1933d51096d62bd77b063f02d
Henrik Jangefelt <Henke9105@hotmail.com>
2022-11-30 20:57:27 +0100

    Class for handling input

---
+++ Game/Source/Handlers/InputHandler.cpp
@@ -0,0 +1,28 @@
+#include "Pch.h"
+#include "InputHandler.h"
+
+
+InputHandler::InputHandler()
+{
+}
+
+InputHandler::~InputHandler()
+{
+}
+
+//void InputHandler::Init(GLFWwindow* aWindow)
+//{
+//	//glfwSetKeyCallback(aWindow, KeyCallback);
+//}
+
+
+//void InputHandler::KeyCallback(GLFWwindow* aWindow, int aKey, int aScanCode, int anAction, int someMods)
+//{
+//	// Key => key pressed
+//	// action => press, release, repeat,
+//
+//	if (aKey == GLFW_KEY_E && anAction == GLFW_PRESS)
+//	{
+//		std::cout << "Press e";
+//	}
+//}
---
+++ Game/Source/Handlers/InputHandler.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Math/Vectors/Vector.hpp"
+
+namespace CU = CommonUtilities;
+
+class InputHandler
+{
+public:
+	InputHandler();
+	~InputHandler();
+
+	//void Init(GLFWwindow* aWindow);
+
+//	static void KeyCallback(GLFWwindow* aWindow, int aKey, int aScanCode, int anAction, int someMods);
+
+private:
+
+};
+
