e4ea08bdbb5dc2e01b246c68ac721bd330a8b5a6
Henrik Jangefelt <Henke9105@hotmail.com>
2022-11-30 20:52:56 +0100

    Added new includes in Precompiled

--- Game/Source/Precompiled/Pch.h
+++ Game/Source/Precompiled/Pch.h
@@ -2,14 +2,23 @@
 
 /* - Standard Library includes - */
 #include <algorithm>
+#include <any>
 #include <array>
+#include <bitset>
 #include <cassert>
 #include <fstream>
 #include <functional>
 #include <iostream>
+#include <memory>
+#include <set>
+#include <sstream>
+#include <stack>
 #include <string>
+#include <typeindex>
+#include <random>
+#include <unordered_map>
+#include <variant>
 #include <vector>
-
 
 /* CommonUtilities */
 #include "../CommonUtilities/Math/Vectors/Vector.hpp"
