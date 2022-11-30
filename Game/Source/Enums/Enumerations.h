4515ea66f853c36d555f4e348e01328e4d1d77f5
Henrik Jangefelt <Henke9105@hotmail.com>
2022-11-30 20:56:16 +0100

    File to store all Enums

--- Game/Source/Enums/Enumerations.h
+++ Game/Source/Enums/Enumerations.h
@@ -7,3 +7,27 @@ enum class eEventType
 	KeyPressed, KeyReleased,
 	MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
 };
+
+/* - Message for components - */
+enum class eMessage // Fix ! (inheritance?) eNotification eMessageType
+{
+	PositionChanged,
+	Colliding,
+
+	Jump,
+	Climb,
+	Slide,
+	MoveUp,
+	MoveDown,
+	MoveLeft,
+	MoveRight,
+	MoveUpLeft,
+	MoveUpRight,
+	MoveDownLeft,
+	MoveDownRight,
+};
+
+enum class eSceneType
+{
+	Title, Loading, Menu, Settings, Game, Pause, GameOver, Count
+};
