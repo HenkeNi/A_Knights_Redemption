02e4bb19764c5ab8ba9883c6e10a09096038e494
Henrik Jangefelt <Henke9105@hotmail.com>
2022-12-01 19:09:17 +0100

    Some Refactoring and added LoadFromFile() function

--- Game/Source/Graphics/Textures/Texture2D.cpp
+++ Game/Source/Graphics/Textures/Texture2D.cpp
@@ -1,2 +1,38 @@
 #include "Pch.h"
 #include "Texture2D.h"
+
+
+Texture2D::Texture2D()
+	: m_id{ 0 }
+{
+	glGenTextures(1, &m_id);
+}
+
+
+void Texture2D::Init(unsigned aWidth, unsigned aHeight, unsigned char* data)
+{
+	// Create Texture
+	glBindTexture(GL_TEXTURE_2D, m_id);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, aWidth, aHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	
+	// Set texture wrap and filter modes
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Unbind texture
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+bool Texture2D::LoadFromFile(const std::string& aFilepath)
+{
+	std::cout << "Loading from path" << aFilepath;
+
+	return true;
+}
+
+void Texture2D::Bind() const
+{
+	glBindTexture(GL_TEXTURE_2D, m_id);
+}
--- Game/Source/Graphics/Textures/Texture2D.h
+++ Game/Source/Graphics/Textures/Texture2D.h
@@ -1,17 +1,17 @@
 #pragma once
+
+
 class Texture2D
 {
 public:
 	Texture2D();
 
-	void Generate(unsigned aWidth, unsigned aHeight, unsigned char* data);
-	void Bind() const;
+
+	void		Init(unsigned aWidth, unsigned aHeight, unsigned char* data); 
+	bool		LoadFromFile(const std::string& aFilepath);
+	
+	void		Bind() const;
 
 private:
-	unsigned m_internalFormat, m_imageFormat;
-	unsigned m_wrapS, m_wrapT;
-	unsigned m_filterMin, m_filterMax;
-	unsigned m_width, m_height;
-	unsigned m_id;
+	unsigned	m_id;
 };
-
