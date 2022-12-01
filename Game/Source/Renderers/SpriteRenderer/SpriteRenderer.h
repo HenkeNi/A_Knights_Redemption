f91479f857a4ef429ec782beed04b72cd561e47f
Henrik Jangefelt <Henke9105@hotmail.com>
2022-12-01 19:12:00 +0100

    Updated spriterenderer

--- Game/Source/Renderers/SpriteRenderer/SpriteRenderer.h
+++ Game/Source/Renderers/SpriteRenderer/SpriteRenderer.h
@@ -1,14 +1,24 @@
 #pragma once
+#include "Shader/Shader.h"
+
+namespace CU = CommonUtilities;
+
+class Texture2D;
 
 class SpriteRenderer
 {
 public:
-	SpriteRenderer();
+	SpriteRenderer(Shader &aShader);
 	~SpriteRenderer();
+	
+	void Init();
+
+	// Store this data in TEXTURE? -> rotation, etc..?
+	void DrawSprite(Texture2D& aTexture, const CU::Vector2<float>& aRotation, const CU::Vector2<float>& aSize, const CU::Vector3<float>& aColor);
 
 private:
-	
 
-	unsigned m_quadVAO;
+	Shader		m_shader;
+	unsigned	m_quadVAO;
 };
 
