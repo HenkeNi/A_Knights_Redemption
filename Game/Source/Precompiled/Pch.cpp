80f7876e06552654f3cf3093ab79c0586ef64c63
Henrik Jangefelt <Henke9105@hotmail.com>
2022-12-09 19:50:54 +0100

    Updated Precompiled

--- Game/Source/Precompiled/Pch.cpp
+++ Game/Source/Precompiled/Pch.cpp
@@ -1,5 +1,8 @@
-// pch.cpp: source file corresponding to the pre-compiled header
+// Pch.cpp: source file corresponding to the pre-compiled header
 
-#include "pch.h"
+#include "Pch.h"
 
 // When you are using pre-compiled headers, this source file is necessary for compilation to succeed.
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
--- Game/Source/Precompiled/Pch.h
+++ Game/Source/Precompiled/Pch.h
@@ -29,3 +29,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <document.h>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp> 
+#include <gtc/type_ptr.hpp>
+#include <stb_image.h>
