6580ac32c05d295754f08a61251ac5c1c860e6bc
Henrik Jangefelt <Henke9105@hotmail.com>
2022-12-09 19:47:56 +0100

    Added a ShaderManager member to the Game class

--- Game/Source/Game.h
+++ Game/Source/Game.h
@@ -1,11 +1,11 @@
 #pragma once
-#include "Pch.h"
 #include "InputHandler.h"
 #include "SceneManager/SceneManager.h"
+#include "SpriteRenderer/SpriteRenderer.h"
 #include "Time/Timer.h"
-#include "Renderers/SpriteRenderer/SpriteRenderer.h"
 #include "ResourceManager/ResourceManager.hpp"
 #include "Window/Window.h"
+
 
 class Game
 {
@@ -17,18 +17,19 @@ public:
 	void ProcessEvents(); // Maybe buffer events (put them in a Queue, and the ProceesEvents at the beginning of each frame?)
 	void Update();
 	void LateUpdate();
-	void Draw()				const;
+	void Draw();		
 	void Shutdown();
-	bool IsRunning()		const;
+	bool IsRunning() const;
 
 private:
 	void RegisterScenes();
 	void MapControlls();
 
 	//ResourceManager<sf::Font, std::string>		m_fonts;
-	//SpriteRenderer								m_spriteRenderer; 
+	SpriteRenderer								m_spriteRenderer; 
 	InputHandler								m_inputHandler;
 	ResourceManager<Texture2D, std::string>		m_textureManager;
+	ResourceManager<Shader, std::string>		m_shaderManager;
 	SceneManager								m_sceneManager;
 	Window										m_window;
 	CU::Timer									m_timer;
