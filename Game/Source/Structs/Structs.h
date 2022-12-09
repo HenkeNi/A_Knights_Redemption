17b0d8364d9252997289d0bf0c62bf98b213640b
Henrik Jangefelt <Henke9105@hotmail.com>
2022-12-09 19:53:39 +0100

    Added a window and sceneManager reference to SharedContext

--- Game/Source/Structs/Structs.h
+++ Game/Source/Structs/Structs.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "SceneManager/SceneManager.h"
+#include "Shader/Shader.h"
+#include "Textures/Texture2D.h"
 #include "Window/Window.h"
 
 
 struct SharedContext
 {
-	Window&			m_window;
-	SceneManager&	m_sceneManager;
+	Window&										m_window;
+	SceneManager&								m_sceneManager;
+	//ResourceManager<Texture2D, std::string>&	m_textureManager;
+	//ResourceManager<Shader, std::string>&		m_shaderManager;
 };
