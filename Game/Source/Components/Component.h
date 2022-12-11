3b81b19096f664525b29bedde52449cde677f7a0
Henrik Jangefelt <Henke9105@hotmail.com>
2022-12-11 14:21:25 +0100

    Added .cpp file, added new functions for activate/deactivate

--- Game/Source/Components/Component.h
+++ Game/Source/Components/Component.h
@@ -2,24 +2,23 @@
 #include "Enumerations.h"
 
 class GameObject;
-class Renderer;
 
 class Component
 {
 public:
-	Component(GameObject* anOwner) : m_owner{ anOwner }, m_isActive{ true } {}
+	Component(GameObject* anOwner);
 	virtual ~Component() = default;
 
 	virtual void	HandleMessage(eMessage aMessageType, const std::any& someData)	= 0;
-	//virtual void	Activate()														= 0;
-	//virtual void	Deactivate()													= 0;
 	virtual void	Update(float aDeltaTime)										= 0;
 	virtual void	LateUpdate(float aDeltaTime)									 {};
 	virtual void	Draw()													   const {};
-	//virtual void	Draw(Window& aWindow)									   const {};
+	virtual void	OnActivate()													 {};
+	virtual void	OnDeactivate()													 {};
 
-	inline  void	SetOwner(GameObject* anOwner)	{ m_owner = anOwner;	 }
-	inline  bool	IsActive() const				{ return m_isActive;	 }
+	bool			IsActive()												   const;
+	void			SetIsActive(bool shouldActivate);
+	void			SetOwner(GameObject* anOwner);
 
 protected:
 	GameObject*		m_owner; // [Consider] replacing with GameObjectID... or a Weak_ptr?
