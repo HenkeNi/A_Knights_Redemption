eaa62dc6bdc511be9cd94ea0c955f8941a14a632
Henrik Jangefelt <Henke9105@hotmail.com>
2022-12-11 13:47:07 +0100

    Updated class with new members
    Added a pointer to Renderer and Transform

--- Game/Source/Components/Sprite/C_Sprite.cpp
+++ Game/Source/Components/Sprite/C_Sprite.cpp
@@ -1,8 +1,47 @@
 #include "Pch.h"
 #include "C_Sprite.h"
+#include "GameObject.h"
+#include "SpriteRenderer/SpriteRenderer.h"
+#include "Transform/C_Transform.h"
 
 
 C_Sprite::C_Sprite(GameObject* anOwner)
-	: Component{ anOwner }, m_texture{ nullptr }
+	: Component{ anOwner }, m_renderer{ nullptr }, m_texture{ nullptr }
 {
+	m_transform = m_owner->GetComponent<C_Transform>(); // TODO: do it in an Active() function?
+}
+
+C_Sprite::C_Sprite(GameObject* anOwner, SpriteRenderer* aRenderer, Texture2D* aTexture)
+	: Component{ anOwner }, m_renderer{ aRenderer }, m_texture{ aTexture }
+{
+	m_transform = m_owner->GetComponent<C_Transform>();
+}
+
+void C_Sprite::HandleMessage(eMessage aMessageType, const std::any& someData)
+{
+	// Listen for position changed (C_Transform)
+
+	// Listen for scale changed (C_Transform)
+}
+void C_Sprite::Update(float aDeltaTime)
+{
+}
+
+void C_Sprite::Draw() const
+{
+	// TODO: Make C_Sprite a friend class instead??
+	// TODO: Have a reference to Transform??
+	//auto transform = m_owner->GetComponent<C_Transform>();
+	auto transform = m_transform.lock();
+	m_renderer->DrawSprite(*m_texture, transform->GetPosition(), transform->GetScale(), {1.f, 1.f, 1.f}, transform->GetRotation());
+}
+
+void C_Sprite::SetRenderer(SpriteRenderer* aRenderer)
+{
+	m_renderer = aRenderer;
+}
+
+void C_Sprite::SetTexture(Texture2D* aTexture)
+{
+	m_texture = aTexture;
 }
--- Game/Source/Components/Sprite/C_Sprite.h
+++ Game/Source/Components/Sprite/C_Sprite.h
@@ -1,16 +1,30 @@
 #pragma once
 #include "Component.hpp"
 
+namespace CU = CommonUtilities;
+
+class C_Transform;
+class SpriteRenderer;
 class Texture2D;
 
 class C_Sprite : public Component
 {
 public:
 	C_Sprite(GameObject* anOwner);
+	C_Sprite(GameObject* anOwner, SpriteRenderer* aRenderer, Texture2D* aTexture);
 
-	 
+	void		HandleMessage(eMessage aMessageType, const std::any& someData)		  override;
+	void		Update(float aDeltaTime)											  override;
+	void		Draw()															const override;
+
+	void		SetRenderer(SpriteRenderer* aRenderer);
+	void		SetTexture(Texture2D* aTexture);
+	// void MirrorSprite() -> Function for inverting Sprite...
+	// void setRenderingRect();
 
 private:
-	Texture2D* m_texture;
+	std::weak_ptr<C_Transform>	m_transform;
+	SpriteRenderer*				m_renderer;
+	Texture2D*					m_texture;
 };
 
