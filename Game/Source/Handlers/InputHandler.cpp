7778a781c0e340aff18279a13c167af876898e75
Henrik Jangefelt <Henke9105@hotmail.com>
2023-01-06 23:05:15 +0100

    Addded some function implementations for the Input handler

--- Game/Source/Handlers/InputHandler.cpp
+++ Game/Source/Handlers/InputHandler.cpp
@@ -4,6 +4,8 @@
 #include "Events/Event.h"
 
 
+std::unordered_map<eInput, eKeyStatus> InputHandler::m_keyStatus;
+
 InputHandler::InputHandler()
 {
 }
@@ -12,22 +14,82 @@ InputHandler::~InputHandler()
 {
 }
 
-void InputHandler::Init()
+void InputHandler::Init(Window* aWindow)
 {
+	m_window = aWindow;
 	//glfwSetKeyCallback(aWindow, KeyCallback);
+}
+
+void InputHandler::ProcessInput()
+{
+	for (auto& status : m_keyStatus)
+	{
+		TranslateInputToEvent(status.first, 1.f); // Pass along if pressed or released???
+	}
+}
+
+void InputHandler::MapEvent(eInput anInput, eEventType aType)
+{
+	m_events[anInput] = aType;
+
 }
 
 void InputHandler::KeyCallback(GLFWwindow* aWindow, int aKey, int aScanCode, int anAction, int someMods)
 {
-	if (aKey == GLFW_KEY_E)
+	switch (aKey)
 	{
-		if (anAction == GLFW_PRESS)
-			;
-			//Dispatcher::GetInstance().SendEvent({ eEventType::KeyPressed, true });
-			//std::cout << "Press e";
-		else if (anAction == GLFW_REPEAT)
-			std::cout << "Repeat e";
-		else if (anAction == GLFW_RELEASE)
-			std::cout << "Release e";
+	case GLFW_KEY_W:
+		m_keyStatus[eInput::Key_W] = GetKeyStatus(anAction);
+		break;
+	case GLFW_KEY_A:
+		m_keyStatus[eInput::Key_A] = GetKeyStatus(anAction);
+		break;
+	case GLFW_KEY_S:
+		m_keyStatus[eInput::Key_S] = GetKeyStatus(anAction);
+		break;
+	case GLFW_KEY_D:
+		m_keyStatus[eInput::Key_D] = GetKeyStatus(anAction);
+		break;
+	case GLFW_KEY_SPACE:
+		m_keyStatus[eInput::Key_Space] = GetKeyStatus(anAction);
+		break;
+	case GLFW_KEY_ESCAPE:
+		m_keyStatus[eInput::Key_Escape] = GetKeyStatus(anAction);
+		break;
 	}
 }
+
+eKeyStatus InputHandler::GetKeyStatus(int anAction)
+{
+	switch (anAction)
+	{
+	case GLFW_PRESS:
+		return eKeyStatus::Pressed;
+	case GLFW_REPEAT:
+		return eKeyStatus::Held;
+	case GLFW_RELEASE:
+		return eKeyStatus::Released;
+	}
+}
+
+void InputHandler::TranslateInputToEvent(eInput anInput, float aValue)
+{
+	if (IsEventMapped(anInput))
+	{
+		Dispatcher::GetInstance().SendEvent(Event{ m_events[anInput], aValue });
+	}
+}
+
+bool InputHandler::IsKeyPressed(eInput anInput)
+{
+	bool isPressed = m_keyStatus[anInput] == eKeyStatus::Pressed;
+	return isPressed;
+}
+
+
+bool InputHandler::IsEventMapped(eInput anInput) const
+{
+	return m_events.find(anInput) != m_events.end();
+
+}
+
--- Game/Source/Handlers/InputHandler.h
+++ Game/Source/Handlers/InputHandler.h
@@ -1,6 +1,7 @@
 #pragma once
+#include "Enumerations.h"
 
-namespace CU = CommonUtilities;
+class Window;
 
 class InputHandler
 {
@@ -8,16 +9,29 @@ public:
 	InputHandler();
 	~InputHandler();
 
-	void		Init();
-	void		ProcessInput();
-	void		MapEvent();
+	void				Init(Window* aWindow);
+	void				ProcessInput();
+	void				MapEvent(eInput anInput, eEventType aType);
 
-	static void KeyCallback(GLFWwindow* aWindow, int aKey, int aScanCode, int anAction, int someMods);
+	static void			KeyCallback(GLFWwindow* aWindow, int aKey, int aScanCode, int anAction, int someMods);
+	static eKeyStatus	GetKeyStatus(int anAction);
+
+	bool				IsKeyPressed(eInput anInput);
 
 private:
+	void				TranslateInputToEvent(eInput anInput, float aValue = 1.0f);
+	bool				IsEventMapped(eInput anInput)										const;
 
 
+	static std::unordered_map<eInput, eKeyStatus>	m_keyStatus; // Renaem s_keyStatus?
+
+	std::unordered_map<eInput, eEventType>			m_events;
+	Window*											m_window;
 
 	// bool keysDown[eKeys::Count], keysPressed[eKeys::Count]
+
+
+	// std::unordered_map<Command, eInput> m_commands;
+
 };
 
